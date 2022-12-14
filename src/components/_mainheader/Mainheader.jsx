4db1f08691a70b8ca7ecd88c255f3aeb38b3f63b
RONALDOE <est.ronaldoernesto@gmail.com>
2022-12-14 15:58:35 -0400

    Comprobar

--- src/components/_mainheader/Mainheader.jsx
+++ src/components/_mainheader/Mainheader.jsx
@@ -1,4 +1,19 @@
 import 'C:\Users\Usuario\Desktop\proyectaso\tempus\src\css\mainheader.css'
 
 
+import React from 'react'
 
+function Mainheader() {
+  return (
+    <div className='header'>
+        <img id='headerLogo' src="C:\Users\Usuario\Desktop\proyectaso\tempus\src\images\logo.svg" alt="Tempus Logo" />
+        <div className='headerThings'>
+            <button id='login'>Ingresar</button>
+            <button id='joinus'>Unete</button>
+        </div>
+         
+    </div>
+  )
+}
+
+export default Mainheader
