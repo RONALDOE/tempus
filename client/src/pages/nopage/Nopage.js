e5188c12f2f6da48dbba790deaff779d451d3c10
RONALDOE <est.ronaldoernesto@gmail.com>
2022-12-25 02:05:15 -0400

    Cambie la jerarquia del proyecto
    Frontend trabaja en client y backend en server

--- client/src/pages/nopage/Nopage.js
+++ client/src/pages/nopage/Nopage.js
@@ -5,9 +5,9 @@ import '../../css/nopage.css'
 export default class Nopage extends Component {
   render() {
     return (
-      <div className="container404">
-        <h1 id="title404">404</h1>
-        <p id="meaning404">Oh no, la pagina no existe</p>
+      <div className="404container">
+        <h1 id="404title">404</h1>
+        <p id="404meaning">Oh no, la pagina no existe</p>
         <Link to='/home'>Regresar</Link>
       </div>
     );
