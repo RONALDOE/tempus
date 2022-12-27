cbd926f87b5eeebafc38feef3343a9520fc414ca
RONALDOE <est.ronaldoernesto@gmail.com>
2022-12-27 13:56:53 -0400

    Resolvi to

---
+++ client/src/pages/singup/Singup.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import '../../css/signup.css'
+
+export default class Signup extends Component {
+  render() {
+    return (
+      <div id="singuppagecontainer">
+        <div className="singupbox">
+            <form className="singupform" action=""
+                autocomplete="off"> 
+                <h2 className="singuph2"> Sign Up </h2>
+                <div className="singupinputBox">
+                    <input className="singupinput" type="text"
+                    required />
+                    <span className="singupspan">Nombre</span><i className="singupi"></i>
+                </div>
+
+                <div className="singupinputBox">
+                    <input className="singupinput" type="email"
+                    required />
+                    <span className="singupspan">E-mail</span><i className="singupi"></i>
+                </div>
+
+                <div class="singupinputBox">
+                    <input className="singupinput" type="password"
+                    required />
+                    <span className="singupspan">Contraseña</span><i className="singupi"></i>
+                </div>
+
+                    <input className="singupinput" type="submit"
+                    value="Registrarse"/>
+            </form>
+        </div> 
+      </div>
+    );
+  }
+}
