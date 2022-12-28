a7ea108c53d2bac85da65b3d3a1fdc6cf0a813f7
RONALDOE <est.ronaldoernesto@gmail.com>
2022-12-28 00:32:18 -0400

    arregle to

--- client/src/pages/nopage/Nopage.js
+++ client/src/pages/nopage/Nopage.js
@@ -1,15 +1,17 @@
-/*import React, { Component } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import '../../css/nopage.css'
+import "../../css/nopage.css";
 
 export default class Nopage extends Component {
   render() {
     return (
       <div className="404container">
-        <h1 id="404title">404</h1>
-        <p id="404meaning">Oh no, la pagina no existe</p>
-        <Link to='/home'>Regresar</Link>
+        <div className="nopageimage" />
+        <div className="nopagetexts">
+          <h3 className="nopageh2">Me parece que estás perdido...</h3>
+          <p>La página que buscas no está habilitada</p>
+        </div>
       </div>
     );
   }
-}*/
+}
